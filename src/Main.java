26fab066972015124fc5d18df660cebcc80c1f07
Antonio Waycon <antoniowaycon@gmail.com>
2025-08-28 19:15:11 -0300

    Criando a classe personagem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args){
+
+        Personagem heroi = new Personagem("Patolino", 20, 100, 70, 100, 1.80, "Mago");
+        Personagem monstro = new Personagem("PernaLonga", 50, 150, 80, 500, 3.0, "Ogro");
+
+        System.out.println("----------------------- HERÓI --------------------------------");
+        System.out.println(heroi.nome);
+        System.out.println(heroi.vida);
+        System.out.println(heroi.classePersonagem);
+        System.out.println("----------------------- MONSTRO --------------------------------");
+        System.out.println(monstro.nome);
+        System.out.println(monstro.vida);
+        System.out.println(monstro.classePersonagem);
+        System.out.println("----------------------------------------------------------------");
     }
+
 }
---
+++ src/Personagem.java
@@ -0,0 +1,43 @@
+public class Personagem {
+    String nome;
+    int idade;
+    int forca;
+    int defesa;
+    int vida;
+    double altura;
+    String classePersonagem;
+
+
+    public Personagem(){
+    }
+
+    public Personagem(String nome, int idade, int forca, int defesa, int vida, double altura, String classePersonagem){
+        this.nome = nome;
+        this.idade = idade;
+        this.forca = forca;
+        this.defesa = defesa;
+        this.vida = vida;
+        this.altura = altura;
+        this.classePersonagem = classePersonagem;
+
+
+    }
+
+    public void atacar(){
+
+    }
+
+    public void defender(){
+
+    }
+
+    public void esquivar(){
+
+    }
+
+    public void correr(){
+
+    }
+
+
+}
